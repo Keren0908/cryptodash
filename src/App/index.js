615b7415defc4cf8f593751f240d61c363a05bfa
keren0908 <akayayy@gmail.com>
2019-01-14 09:48:52 -0500

    Add Loading Content

--- src/App/index.js
+++ src/App/index.js
@@ -7,6 +7,7 @@ import './App.css';
 import {AppProvider} from './AppProvider';
 import AppBar from './AppBar';
 import Settings from '../Settings';
+import Content from '../Shared/Content';
 
 
 
@@ -16,7 +17,9 @@ class App extends Component {
       <AppLayout>
         <AppProvider>
           <AppBar />
-          <Settings />
+          <Content>
+            <Settings />
+          </Content>
         </AppProvider>
       </AppLayout>
     );
