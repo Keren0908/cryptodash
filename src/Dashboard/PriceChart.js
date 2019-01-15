392834c8b79c4670bca8d97f20f0cf0e1d872ce4
keren0908 <akayayy@gmail.com>
2019-01-15 14:33:09 -0500

    Add ChartSelect

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,19 +1,32 @@
-import HighChartsConfig from './HighChartsConfig';
-import React from 'react';
-import {Tile} from '../Shared/Tile';
-import {AppContext} from '../App/AppProvider';
-import ReactHighcharts from 'react-highcharts';
-import HighChartsTheme from './HighChartsTheme';
+import HighChartsConfig from "./HighChartsConfig";
+import React from "react";
+import { Tile } from "../Shared/Tile";
+import { AppContext } from "../App/AppProvider";
+import ReactHighcharts from "react-highcharts";
+import HighChartsTheme from "./HighChartsTheme";
+import ChartSelect from "./ChartSelect";
 
 ReactHighcharts.Highcharts.setOptions(HighChartsTheme);
-export default function(){
-    return(
-        <AppContext.Consumer>
-            {({historical}) => 
-                <Tile>
-                    {historical ? <ReactHighcharts config={HighChartsConfig(historical)} /> : <div> Loading Historical Data </div>}
-                </Tile>
-            }
-        </AppContext.Consumer>
-    )
+export default function() {
+  return (
+    <AppContext.Consumer>
+      {({ historical, changeChartSelect }) => (
+        <Tile>
+          <ChartSelect
+            defaultValue={"months"}
+            onChange={e => changeChartSelect(e.target.value)}
+          >
+            <option value="days"> Days </option>
+            <option value="weeks"> Weeks </option>
+            <option value="months"> Months </option>
+          </ChartSelect>
+          {historical ? (
+            <ReactHighcharts config={HighChartsConfig(historical)} />
+          ) : (
+            <div> Loading Historical Data </div>
+          )}
+        </Tile>
+      )}
+    </AppContext.Consumer>
+  );
 }
