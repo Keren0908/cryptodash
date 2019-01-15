8d57fbfc0f8372766e6e9b07df2e18fc146319e5
keren0908 <akayayy@gmail.com>
2019-01-15 14:47:13 -0500

    Updates

---
+++ precache-manifest.feb006a3ffbab42251cb0860b1d79a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf01cf5f920dea0efeda",
+    "url": "/cryptoDash/static/js/runtime~main.bf01cf5f.js"
+  },
+  {
+    "revision": "6b7495c4bd3f11c4e1ec",
+    "url": "/cryptoDash/static/js/main.6b7495c4.chunk.js"
+  },
+  {
+    "revision": "ab43d8350a60702b0a67",
+    "url": "/cryptoDash/static/js/1.ab43d835.chunk.js"
+  },
+  {
+    "revision": "6b7495c4bd3f11c4e1ec",
+    "url": "/cryptoDash/static/css/main.42bface7.chunk.css"
+  },
+  {
+    "revision": "cc1904962ca22c808a012181ad918afa",
+    "url": "/cryptoDash/index.html"
+  }
+];
