7dcdd01f7e0d2717daab1c74ee3573d1aad61881
keren0908 <akayayy@gmail.com>
2019-01-15 00:18:23 -0500

    Clear the search box then return all the coins

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -39,6 +39,10 @@ const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
 
 function filterCoins(e, setFilteredCoins, coinList){
     let inputValue = e.target.value;
+    if(!inputValue){
+        setFilteredCoins(null);
+        return;
+    }
     handleFilter(inputValue, coinList, setFilteredCoins);
 }
 
