4fd175a6f8e06ed957f0aa05b23c27e0362bcfd4
keren0908 <akayayy@gmail.com>
2019-01-15 14:13:31 -0500

    Fix bugs

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
-import {AppContext} from '../App/AppProvider';
-import CoinTile from './CoinTile';
+import React from "react";
+import styled from "styled-components";
+import { AppContext } from "../App/AppProvider";
+import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
-    grid-gap: 15px;
-    margin-top: 40px;
-`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+  grid-gap: 15px;
+  margin-top: 40px;
+`;
 
-function getLowerSectionCoins(filteredCoins,coinList){
-    //console.log(coinList);
-    return filteredCoins && Object.keys(filteredCoins) || Object.keys(coinList).slice(0, 100);
+function getLowerSectionCoins(filteredCoins, coinList) {
+  //console.log(coinList);
+  return (
+    (filteredCoins && Object.keys(filteredCoins)) ||
+    Object.keys(coinList).slice(0, 100)
+  );
 }
 
-function getCoinsToDisplay(coinList, topSection, favorites, filteredCoins){
-    return topSection ? favorites : getLowerSectionCoins(filteredCoins,coinList);//Object.keys(coinList).slice(0, 100);
+function getCoinsToDisplay(coinList, topSection, favorites, filteredCoins) {
+  return topSection ? favorites : getLowerSectionCoins(filteredCoins, coinList); //Object.keys(coinList).slice(0, 100);
 }
 
-export default function({topSection}){
-    return (
+export default function({ topSection }) {
+  return (
     <AppContext.Consumer>
-        {({coinList, favorites, filteredCoins}) => <CoinGridStyled>
-                {getCoinsToDisplay(coinList, topSection, favorites,filteredCoins).map(coinKey => 
-                    <CoinTile topSection={topSection} key={coinKey} coinKey={coinKey} />)}
-            </CoinGridStyled>}
+      {({ coinList, favorites, filteredCoins }) => (
+        <CoinGridStyled>
+          {getCoinsToDisplay(
+            coinList,
+            topSection,
+            favorites,
+            filteredCoins
+          ).map(coinKey => (
+            <CoinTile topSection={topSection} key={coinKey} coinKey={coinKey} />
+          ))}
+        </CoinGridStyled>
+      )}
     </AppContext.Consumer>
-    )
+  );
 }
